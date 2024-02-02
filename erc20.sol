4ad9eff337960f8bf0ee38d400d116948065913c
mohdSafwan123 <157371662+mohdSafwan123@users.noreply.github.com>
2024-02-02 20:16:57 +0530

    Create erc20.sol

---
+++ erc20.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyUpdatedToken is ERC20 {
+    address public tokenOwner;
+
+    modifier onlyTokenOwner() {
+        require(msg.sender == tokenOwner, "Not the owner");
+        _;
+    }
+
+    constructor() ERC20("MyUpdatedToken", "MUT") {
+        tokenOwner = msg.sender;
+        _mint(msg.sender, 1000000 * 10**decimals());
+    }
+
+    function mintTokens(address to, uint256 amount) external onlyTokenOwner {
+        _mint(to, amount);
+    }
+
+    function burnTokens(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+}
